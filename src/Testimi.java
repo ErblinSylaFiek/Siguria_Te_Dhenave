cec45bdf4c162cd6931cab34c65bdae5f31fd105
Erblin Syla <erblin.sylaa@gmail.com>
2025-04-16 20:46:05 +0200

    Shtimi i fjalive me simbole per testim

--- src/Testimi.java
+++ src/Testimi.java
@@ -1,5 +1,9 @@
 public class Testimi {
     public static void main(String[] args){
+        String fjaliMeSimbole = "Pershendetje123.Une jam Erblin Syla@#";
+        int shift = 5;
 
+        String enkriptimiSimbol = Enkriptimi.encrypt(fjaliMeSimbole, shift);
+        String dekriptimiSimbol = Dekriptimi.decrypt(enkriptimiSimbol, shift);
     }
 }
