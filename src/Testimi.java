9596021dd743382c104ac90525d052ae4bb5f259
Erblin Syla <erblin.sylaa@gmail.com>
2025-04-16 20:50:01 +0200

    Testimi per simbole kalon me sukses!

--- src/Testimi.java
+++ src/Testimi.java
@@ -5,5 +5,15 @@ public class Testimi {
 
         String enkriptimiSimbol = Enkriptimi.encrypt(fjaliMeSimbole, shift);
         String dekriptimiSimbol = Dekriptimi.decrypt(enkriptimiSimbol, shift);
+
+        System.out.println("Teksti me simbole: " + fjaliMeSimbole);
+        System.out.println("Teskti i enkriptuar me shift " + shift + ": " + enkriptimiSimbol);
+        System.out.println("Teksi i dekriptuar: " + dekriptimiSimbol);
+
+        if (fjaliMeSimbole.equals(dekriptimiSimbol)) {
+            System.out.println("Testimi per simbole kaloi me sukses, simbolet nuk jane ndryshu!");
+        } else {
+            System.out.println("Testi deshtoi, simbolet jane ndryshu!");
+        }
     }
 }
